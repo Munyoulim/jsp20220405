ec17fa93f9015a3955c787cfab58043e24842c89
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-04-14 13:07:14 +0900

    220414

--- src/chap11/Student.java
+++ src/chap11/Student.java
@@ -1,5 +1,13 @@
 package chap11;
 
+// Student 클래스를 사용하고 싶은데
+// 생성자 선언하지 않으면 사용할수가 없다. (필드 생성 후 사용하고 싶으면 생성자 선언 필수)
+// 만약 파라미터 없는 생성자만 필요한 경우,
+// 자동으로 추가 되기때문에 사용에 문제가 없지만..
+// 파라미터 있는 생성자가 필요할 경우에는,
+// !!! 파라미터 없는 생성자가 자동으로 추가가 되지 않기 때문에 !!!
+// 파라미터 있는 생성자, 파라미터 없는 생성자 둘다 선언해줘야 사용가능하다.
+
 public class Student {
 	private String name;
 	private String gender;
@@ -38,5 +46,4 @@ public class Student {
 	public void setScore(int score) {
 		this.score = score;
 	}
-
 }
